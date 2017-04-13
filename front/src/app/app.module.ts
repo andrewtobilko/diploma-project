f101a47442c94baafb8904c11efdf1960e1a5b99
Yehor123 <ekrivokon@inbox.ru>
2017-04-13 13:58:48 +0300

    added settings-component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {SettingsComponent} from "./app.settings-component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
