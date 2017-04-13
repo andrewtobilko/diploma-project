c31ccbd4e0642d7dbb513062169bb0747cd0410b
Yehor123 <ekrivokon@inbox.ru>
2017-04-13 15:10:43 +0300

    added routers.ts now settings component shows by clicking on the Settings button

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {SettingsComponent} from "./app.settings-component";
+import {APPLICATION_ROUTERS} from "app/app.routers";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import {SettingsComponent} from "./app.settings-component";
     SettingsComponent
   ],
   imports: [
+    RouterModule.forRoot(APPLICATION_ROUTERS),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ front/src/app/app.routers.ts
@@ -0,0 +1,9 @@
+import {Routes} from "@angular/router";
+import {SettingsComponent} from "./app.settings-component";
+
+export const APPLICATION_ROUTERS: Routes = [
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
