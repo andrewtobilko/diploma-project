d902f394a9ab81148ed8d7051bbb05bfd7829952
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-04-24 13:59:30 +0300

    logger server

--- src/main/java/com/tobilko/logging/ServerLoggingService.java
+++ src/main/java/com/tobilko/logging/ServerLoggingService.java
@@ -1,21 +1,28 @@
 package com.tobilko.logging;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.io.File;
 import java.util.List;
+
+import static java.nio.file.Files.lines;
+import static java.nio.file.Paths.get;
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class ServerLoggingService {
 
+    private final String LOGGER_FILE_LOCATION = "C:\\Users\\andre\\IdeaProjects\\diploma-project\\server.log";
+
     @Transactional
     public List<String> fetchLatestLoggerMessages() {
-        return fetchLatestLoggerMessagesFromFile(new File(""));
+        return fetchLatestLoggerMessagesFromFile();
     }
 
-    private List<String> fetchLatestLoggerMessagesFromFile(File file) {
-        return null;
+    @SneakyThrows
+    private List<String> fetchLatestLoggerMessagesFromFile() {
+        return lines(get(LOGGER_FILE_LOCATION)).collect(toList());
     }
 
 }
